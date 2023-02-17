9fb09c57f97d1185e5147b24521ba2a9d0b4afff
Thomas Cipriani <the.beel.616@gmail.com>
2023-02-17 16:36:47 +0100

    Use nextJS api for joke request

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -2,10 +2,17 @@ import { Joke, Page } from "@aquacloud/internal";
 import { useQuery } from "@tanstack/react-query";
 import { NextPage } from "next";
 
+const fetchJokes = async (page: number = 1) => {
+  return (await (
+    await fetch("/api/jokes?" + new URLSearchParams({ page: page.toString() }))
+  ).json()) as Page<Joke>;
+};
+
 const Page: NextPage = () => {
+  const page: number = 1;
   const { data: jokes } = useQuery<Page<Joke>>({
-    queryKey: ["jokes"],
-    queryFn: async () => (await fetch("/api/jokes")).json(),
+    queryKey: ["jokes", page],
+    queryFn: () => fetchJokes(page),
   });
 
   return (
