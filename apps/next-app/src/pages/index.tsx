3538a383cb590d068f3796aa2578eed4b583037d
Thomas Cipriani <the.beel.616@gmail.com>
2023-02-18 19:13:05 +0100

    Used List UI component in HomePage

--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Joke, Page } from "@aquacloud/internal";
+import { List } from "@aquacloud/ui";
 import { useQuery } from "@tanstack/react-query";
 import { NextPage } from "next";
 
@@ -17,16 +18,14 @@ const Page: NextPage = () => {
 
   return (
     <main className="min-h-screen bg-slate-200 py-2 text-slate-800 dark:bg-slate-800 dark:text-slate-200">
-      <div className="container mx-auto flex flex-col gap-2">
-        {jokes &&
-          jokes.data.map((joke) => (
-            <div
-              key={joke.id}
-              className="rounded border border-slate-800/70 p-1 dark:border-slate-200/70"
-            >
-              {joke.value}
-            </div>
-          ))}
+      <div className="container mx-auto flex flex-col">
+        {jokes && (
+          <List>
+            {jokes.data.map((joke) => (
+              <List.Item key={joke.id}>{joke.value}</List.Item>
+            ))}
+          </List>
+        )}
       </div>
     </main>
   );
