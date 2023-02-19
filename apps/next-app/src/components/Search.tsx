53cc2c450ca9d8a62cf17c714c1a16b2f2efbc1b
Thomas Cipriani <the.beel.616@gmail.com>
2023-02-19 16:21:11 +0100

    Created Search component

---
+++ apps/next-app/src/components/Search.tsx
@@ -0,0 +1,32 @@
+import { Button, TextInput } from "@aquacloud/ui";
+import { FormEvent, useState } from "react";
+
+interface SearchProps {
+  minLength?: number;
+  onSearch?: (term: string) => void;
+}
+
+const Search = ({ minLength = 3, onSearch }: SearchProps) => {
+  const [term, setTerm] = useState("");
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSearch && onSearch(term);
+  };
+
+  return (
+    <form className="flex gap-2" onSubmit={handleSubmit}>
+      <TextInput
+        className="grow"
+        placeholder="Search..."
+        minLength={minLength}
+        value={term}
+        onChange={(e) => setTerm(e.currentTarget.value)}
+      />
+      <Button type="submit" intent="info" disabled={term.length < minLength}>
+        Search
+      </Button>
+    </form>
+  );
+};
+
+export default Search;
