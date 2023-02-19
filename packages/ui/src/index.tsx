caaf9cff9aa998bf00d0436b9db8a4559c7a635e
Thomas Cipriani <the.beel.616@gmail.com>
2023-02-20 00:01:32 +0100

    Create Select UI component

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,3 +2,4 @@ export * from "./components/List";
 export * from "./components/Alert";
 export * from "./components/TextInput";
 export * from "./components/Button";
+export * from "./components/Select";
