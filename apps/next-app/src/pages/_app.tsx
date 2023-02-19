c6b8ccc38b3a6383b5377d1a6366f7601de88d95
Thomas Cipriani <the.beel.616@gmail.com>
2023-02-20 00:08:03 +0100

    Created useDarkMode hook

--- apps/next-app/src/pages/_app.tsx
+++ apps/next-app/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import useDarkMode from "@/utils/useDarkMode";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import type { AppProps } from "next/app";
@@ -6,6 +7,8 @@ import type { AppProps } from "next/app";
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const darkMode = useDarkMode();
+
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
